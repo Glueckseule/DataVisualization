924f82388e442f93a2eddbede5251df79b87349c
Glueckseule <30928455+Glueckseule@users.noreply.github.com>
2018-04-30 15:59:10 +0200

    read in JSON for latest season

---
+++ code.js
@@ -0,0 +1,16 @@
+readTextFile("json_files/saison_16_17.json", function(text){
+    var data = JSON.parse(text);
+    console.log(data);
+});
+
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
