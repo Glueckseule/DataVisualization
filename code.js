dcff99bebbf806cb5df83444357c3f31b2673516
Glueckseule <30928455+Glueckseule@users.noreply.github.com>
2018-05-01 13:55:03 +0200

    changed syntax for to forEach; save value from dropdown to currentTeam variable

--- code.js
+++ code.js
@@ -1,72 +1,72 @@
-var d3 = d3 || {};
-var data;
-var teams = new Array();
+var d3 = d3 || {}
+var data
+var teams = new Array()
+var currentTeam
 
 //HS und AS Torversuche nicht aufs Tor
 //HST und AST haben getroffen oder hätten ohne Torwart getroffen
 //HHW und AHW sind Latten und Pfosten
 
 function doChart(){
-  "use strict";
+  "use strict"
 
   var margin = {top: 20, right: 20, bottom: 70, left: 40},
-      width = 600-margin.left-margin.right, height = 300-margin.top-margin.bottom;
+      width = 600-margin.left-margin.right, height = 300-margin.top-margin.bottom
 
-  var x = d3.scaleBand().rangeRound([0, width]).padding([.05]);
-  var y = d3.scaleLinear().range([height, 0]);
+  var x = d3.scaleBand().rangeRound([0, width]).padding([.05])
+  var y = d3.scaleLinear().range([height, 0])
 
   var xAxis = d3.axisBottom()
-                .scale(x);
+                .scale(x)
 
   var yAxis = d3.axisLeft()
                 .scale(y)
-                .ticks(5);
+                .ticks(5)
 
   var svg = d3.select(".chart")
               .attr("width", width + margin.left + margin.right)
               .attr("height", height + margin.bottom + margin.top)
               .append("g")
-              .attr("transform", "translate(" + margin.left + "," + margin.top+")");
+              .attr("transform", "translate(" + margin.left + "," + margin.top+")")
 
   d3.json("json_files/saison_16_17.json", function(error, json){
-    if(error) return console.warn(error);
+    if(error) return console.warn(error)
 
-    data = json;
-    console.log(data["saison_16_17"][0]);
+    data = json
+    console.log(data["saison_16_17"][0])
 
-    extractTeams();
-    makeDropdown();
+    extractTeams()
+    makeDropdown()
 
-    console.log(teams);
-
-
-  });
+  })
 }
 
 function extractTeams(){
-  for(let i = 0; i < data["saison_16_17"].length; i++){
-    if(!teams.includes(data["saison_16_17"][i]["AwayTeam"])){
-      teams.push(data["saison_16_17"][i]["AwayTeam"]);
+  data["saison_16_17"].forEach((e, i) => {
+    if(!teams.includes(e["AwayTeam"])){
+      teams.push(data["saison_16_17"][i]["AwayTeam"])
     }
-  }
+  })
 }
 
 function makeDropdown(){
-  var select = d3.select('.menuDropdown')
-                 .append('select')
-                 .attr('class', 'select')
-                 .on('change', onChangeTeam);
+  var select = d3.select('.select')
+                 .on('change', onChangeTeam)
 
   var options = select.selectAll('option')
-                      .data(teams).enter()
+                      .data(teams)
+                      .enter()
                       .append('option')
                       .text(function(d){
-                        return d;
-                      });
+                        return d
+                      })
+                      .attr('value', function(d){
+                        return d
+                      })
 
   function onChangeTeam(){
-    
+    currentTeam = this.value
   }
 }
 
-doChart();
+doChart()
